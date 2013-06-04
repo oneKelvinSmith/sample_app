08e3b0117bd4b357acc6f9d4b6abe5d735d64bd4
Kelvin Smith <ksmith@thoughtworks.com>
2013-06-04 22:46:09 +0200

    Refactor requests specs and use form_tag in user signup view.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,23 +28,11 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-
-      describe "after submission" do
-        before { click_button submit }
-
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-        # put in tests for specific error strings
-      end
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      let(:user) { FactoryGirl.build(:user) }
+      before { valid_signup(user) }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -52,11 +40,10 @@ describe "UserPages" do
 
       describe "after saving the user" do
         before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it  {should have_success_message('Welcome')}
       end
     end
   end

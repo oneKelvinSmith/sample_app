d112e1b6aa3f8f1bf6ae6a76acc52067aa5e7a0b
Kelvin Smith <ksmith@thoughtworks.com>
2013-06-06 18:53:32 +0200

    Finish section 9.2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,2 +1,17 @@
 include ApplicationHelper
-include RequestsHelper
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+def sign_up(user)
+  fill_in "Name",         with: user.name
+  fill_in "Email",        with: user.email
+  fill_in "Password",     with: user.password
+  fill_in "Confirmation", with: user.password_confirmation
+end
